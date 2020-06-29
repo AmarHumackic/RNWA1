79c91863a508d0aaf206586942e0b84fc88f46fa
Amar <amar.humackic@edu.fit.ba>
2020-06-29 14:12:46 +0200

    Add Web Service

---
+++ FSREWebService/WebServis1.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WebServis1.asmx.cs" Class="FSREWebService.WebServis1" %>
---
+++ FSREWebService/WebServis1.asmx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace FSREWebService
+{
+    /// <summary>
+    /// Summary description for WebServis1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebServis1 : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        [WebMethod]
+        public int GetSquare(int a)
+        {
+            return (a * a);
+        }
+
+        [WebMethod]
+        public DataSet GetOrdersByID(int userId)
+        {
+            DataSet ds = new DataSet();
+
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["connWebService"].ToString();
+
+            using (SqlCommand cmd = new SqlCommand("uspGetOrdersByID", conn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.Add("@customerNumber", SqlDbType.Int).Value = userId;
+
+                conn.Open();
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                adapter.Fill(ds);
+                conn.Close();
+            }
+            return ds;
+        }
+    }
+}
